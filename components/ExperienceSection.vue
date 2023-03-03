3ae0489ebac11ed5c5a63df33629c767c5c19d32
naukhaj <nikhil.aukhaj@prodigious.mu>
2023-03-03 22:30:41 +0400

    Added Experience Section.

---
+++ components/ExperienceSection.vue
@@ -0,0 +1,33 @@
+<template>
+    <section class="experience">
+        <div class="experience-wrapper">
+            <h1>My Experience</h1>
+            <div class="experience-img">
+                <img src="" alt="University Image">
+            </div>
+            <div class="experience-text">
+                <div class="experience-timeline">
+                    <div class="experience-timeline-year">1000</div>
+                    <div class="experience-timeline-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, corporis!</div>
+                </div>
+                <div class="experience-timeline">
+                    <div class="experience-timeline-year">1000</div>
+                    <div class="experience-timeline-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, corporis!</div>
+                </div>
+                <div class="experience-timeline">
+                    <div class="experience-timeline-year">1000</div>
+                    <div class="experience-timeline-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, corporis!</div>
+                </div>
+            </div>
+        </div>
+       
+    </section>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
