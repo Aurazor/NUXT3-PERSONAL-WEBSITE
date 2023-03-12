0004dd94cab266b7481b74e97efac09ff6993be4
Aurazor <nikhilaukhaj590@gmail.com>
2023-03-12 16:32:45 +0400

    More changes.

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
         <!-- This is the front page -->
         <FrontCover/>
         <!-- This is the expertise page -->
+        <!-- More changes from remote -->
         <ExpertiseSection/>
         <ExperienceSection/>
         <Footer/>
